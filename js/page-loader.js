28c23e26a757435e76498b33f8b8e28d50295fd9
Ritik3692 <ritikkashyap464@gmail.com>
2024-09-19 19:15:11 +0530

    first commit

--- js/page-loader.js
+++ js/page-loader.js
@@ -1,3 +1,15 @@
+// Simulate loading process and hide the loader
+//  window.addEventListener("load", function () {
+//     var loader = document.getElementById("loader");
+//     var content = document.getElementById("content");
+
+//     // Hide loader after 3 seconds and show content
+//     setTimeout(function () {
+//         loader.style.display = "none";
+//         content.style.display = "block";
+//     }, 3000); // Adjust time if necessary
+// });
+
 
 let loaders = document.querySelector("#loader");
 
